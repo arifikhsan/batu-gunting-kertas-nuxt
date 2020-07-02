4ddab3676da5dbad5d64a89960ae4ee3d6a23887
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-07-02 20:28:25 +0700

    done

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@
     <div class="flex items-center justify-center flex-col text-center">
       <div class="p-4">
         <h1 class="text-3xl font-bold text-indigo-500">Batu Gunting Kertas</h1>
-        <h2 class="text-base text-indigo-500">Dengan Kecerdasan Buatan</h2>
+        <h2 class="text-base  text-indigo-500">Dengan Kecerdasan Buatan</h2>
       </div>
-      <div class="p-4 mt-4">
+      <div class="p-4">
         <h2 class="text-2xl font-semibold text-indigo-500">Skor</h2>
         <div class="mt-4 flex items-row items-center justify-evenly">
           <div class="w-1/2 border-r border-indigo-500">
@@ -19,7 +19,12 @@
             <p class="mt-4">{{ showChosenByAI }}</p>
           </div>
         </div>
-        <div class="my-6 text-2xl font-bold">{{ showWinner }}</div>
+        <div class="my-6 text-2xl font-bold">
+          <p class="text-indigo-500" v-if="this.winner === 'human'">Anda menang</p>
+          <p class="text-red-500" v-else-if="this.winner === 'AI'">Anda kalah</p>
+          <p class="text-blue-500" v-else-if="this.winner === 'draw'">Seri</p>
+          <p class="text-gray-700" v-else>Mulai Permainan</p>
+        </div>
         <div class="mt-4">
           <div class="flex flex-row items-center justify-center">
             <button
@@ -49,7 +54,9 @@
               Reset
             </button>
           </div>
-          game ke: {{ gameCount }}
+          <div class="mt-8">
+            <p>Game ke: {{ gameCount }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -70,7 +77,7 @@ export default {
       chosenByAI: 0,
       winner: '', // human or AI or draw
       gameCount: 0,
-      patternLength: 6
+      patternLength: 10
     }
   },
   computed: {
@@ -87,10 +94,10 @@ export default {
       }
     },
     showChosenByHuman() {
-      return this.chosenByHuman
+      return this.stringOf(this.chosenByHuman)
     },
     showChosenByAI() {
-      return this.chosenByAI
+      return this.stringOf(this.chosenByAI)
     }
   },
   methods: {
@@ -164,6 +171,23 @@ export default {
       this.chosenByAI = 0
       this.winner = ''
       this.gameCount = 0
+    },
+    stringOf(integer) {
+      switch (integer) {
+        case 1:
+          return 'Batu'
+          break
+        case 2:
+          return 'Gunting'
+          break
+        case 3:
+          return 'Kertas'
+          break
+
+        default:
+          return ''
+          break
+      }
     }
   }
 }
