0e01725fa5900b4c44761515fe3b695822871c06
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-07-02 14:50:03 +0700

    text center

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-5xl mx-auto">
-    <div class="flex items-center justify-center flex-col">
+    <div class="flex items-center justify-center flex-col text-center">
       <div class="p-12">
         <h1 class="text-3xl font-bold text-indigo-500">Batu Gunting Kertas</h1>
       </div>
