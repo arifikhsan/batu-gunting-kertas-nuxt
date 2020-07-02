0fd5ac1608839aeb0f8ea80074e2ebad407d4b0a
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-07-02 15:12:48 +0700

    up

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,38 @@
 <template>
   <div class="max-w-5xl mx-auto">
     <div class="flex items-center justify-center flex-col text-center">
-      <div class="p-12">
+      <div class="p-4 mt-6">
         <h1 class="text-3xl font-bold text-indigo-500">Batu Gunting Kertas</h1>
       </div>
-      <div class="p-12">Gamenya disini</div>
+      <div class="p-4 mt-8">
+        <div class="flex items-row items-center justify-evenly">
+          <div class="w-1/2 border-r border-indigo-500">
+            <p class="text-xl">Manusia</p>
+            <p class="mt-4">Pilihan</p>
+          </div>
+          <div class="w-1/2">
+            <p class="text-xl">AI</p>
+            <p class="mt-4">Pilihan</p>
+          </div>
+        </div>
+        <div class="mt-12 flex flex-row items-center justify-center">
+          <button
+            class="py-2 m-2 px-4 text-white bg-indigo-500 rounded hover:bg-indigo-600 duration-500"
+          >
+            Batu
+          </button>
+          <button
+            class="py-2 m-2 px-4 text-white bg-indigo-500 rounded hover:bg-indigo-600 duration-500"
+          >
+            Gunting
+          </button>
+          <button
+            class="py-2 m-2 px-4 text-white bg-indigo-500 rounded hover:bg-indigo-600 duration-500"
+          >
+            Kertas
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
