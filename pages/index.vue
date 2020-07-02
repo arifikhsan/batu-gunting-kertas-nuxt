41e3caeb1192f442b36fc3263e6f80b129eb5e86
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-07-02 14:48:36 +0700

    title

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold">Batu Gunting Kertas</h1>
+  <div class="max-w-5xl mx-auto">
+    <div class="flex items-center justify-center flex-col">
+      <div class="p-12">
+        <h1 class="text-3xl font-bold text-indigo-500">Batu Gunting Kertas</h1>
+      </div>
+      <div class="p-12">Gamenya disini</div>
+    </div>
   </div>
 </template>
 
